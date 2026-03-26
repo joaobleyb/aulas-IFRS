55de9442ed20514159687cd3410e6246ecfb133c
joaobleyb <joaobleydev@gmail.com>
2026-03-26 15:04:22 -0300

    testes prontos do test_cripto

--- teste-de-qualidade/test_cripto_cesar.py
+++ teste-de-qualidade/test_cripto_cesar.py
@@ -31,3 +31,53 @@ def test_ultima_letra_alfabeto():
 
 def test_somenteSimbolos():
     assert cripto_cesar("!@$%&*", 3) == "!@$%&*"
+
+def test_k_grande():
+    assert cripto_cesar("abc", 27) == "bcd"
+
+# Exceções:
+
+def test_mensagem_nao_string():
+    with pytest.raises(TypeError):
+        cripto_cesar(123, 1)
+
+def test_k_nao_inteiro():
+    with pytest.raises(TypeError):
+        cripto_cesar("abc", "1")
+
+def test_mensagem_none():
+    with pytest.raises(TypeError):
+        cripto_cesar(None, 3)
+
+# Casos Tipicos com parametrize: (Mesmos Casos de Tipicos acima, mas usando parametrize)
+
+@pytest.mark.parametrize("mensagem,k,esperado", [
+    ("ABC", 3, "DEF"),             # Maiúsculas
+    ("abc", 3, "def"),             # Minúsculas
+    ("AbC", 3, "DeF"),             # Mistura de maiúsculas e minúsculas
+    ("Oi 123!", 5, "Tn 123!"),     # Caracteres não alfabéticos e Números
+])
+def test_cripto_cesar_casos_tipicos(mensagem, k, esperado):
+    assert cripto_cesar(mensagem, k) == esperado
+
+#Casos de borda com parametrize: (Mesmos Casos de Bordas acima, mas usando parametrize)
+
+@pytest.mark.parametrize("mensagem,k,esperado", [
+    ("", 5, ""),              # String vazia
+    ("abc", 0, "abc"),        # k = 0
+    ("bcd", -1, "abc"),       # k negativo
+    ("abc", 27, "bcd"),       # k maior que 26
+])
+def test_cripto_cesar_borda(mensagem, k, esperado):
+    assert cripto_cesar(mensagem, k) == esperado
+
+#Exceções com parametrize: (Mesmos Casos de Exceções acima, mas usando parametrize)
+
+@pytest.mark.parametrize("mensagem,k,esperado", [
+    (123, 1, "TypeError"),        # Mensagem não é string
+    ("abc", "1", "TypeError"),     # k não é inteiro
+    (None, 3, "TypeError"),        # Mensagem é None
+])
+def test_cripto_cesar_excecoes(mensagem, k, esperado):
+    with pytest.raises(TypeError):
+        cripto_cesar(mensagem, k)
